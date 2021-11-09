1a4fd98ebfc50f641f7ad6608dfc539ced61453d
Karan Sodhi <karanpreetsodhi1997@gmail.com>
2021-11-09 14:11:53 -0500

    displaying different questions one after other and chcking their respective answers

--- index.js
+++ index.js
@@ -1,28 +1,29 @@
 var timerEl = document.querySelector(".timers")
 var startQuizEl = document.querySelector("button")
 var mainWrapEl = document.querySelector(".main-wrap")
+var o = -1;
 var listQUestions = [//list of question objects with correct answer
     {
-        question:"A very important tool ",
+        question:"A very important tools ",
         options:["h1","p","footer","img"],
         rightAnswer : function(){
             return this.options[0]}
     }
-    // },
-    // {
-    //     question:"What is the tag for link?",
-    //     options:["h1","p","footer","a"],
-    //     rightAnswer : function(){
-    //         return this.options[3]
-    //     }
-    // },
-    // {
-    //     question:"What is the tag for paragraph?",
-    //     options:["h1","p","footer","img"],
-    //     rightAnswer : function(){
-    //         return this.options[1]
-    //     }
-    // }
+    ,
+    {
+        question:"What is the tag for link?",
+        options:["h1","p","footer","a"],
+        rightAnswer : function(){
+            return this.options[3]
+        }
+    },
+    {
+        question:"What is the tag for paragraph?",
+        options:["h1","p","footer","img"],
+        rightAnswer : function(){
+            return this.options[1]
+        }
+    }
 
 ]
 //keeps check on how much time is left 
@@ -52,11 +53,12 @@ function displayQuestions(){
     //dynamically create div with question and options inside of it
     //replace the main screen with the question
 
-
+    o++;
 
     var questionWrapperEl = document.createElement("div");
-    questionWrapperEl.className = "questionWrapper";
-
+    questionWrapperEl.className = "mainView"
+    questionWrapperEl.id = "questionWrapper";
+    
     var questionEl = document.createElement("p");
     questionEl.className = "questionEl"
 
@@ -64,22 +66,22 @@ function displayQuestions(){
     optionsEl.className = "optionsEl"
     
 
-    for(o in listQUestions){
+    
         //looping through questionObject List
+    if(o < listQUestions.length){
         questionEl.textContent = listQUestions[o].question;
         for(i in listQUestions[o].options){
             var option = document.createElement("li");
             option.className = "options"
-            option.textContent = listQUestions[o].options[i]
+            option.textContent = parseInt(i) + 1 + ". " + listQUestions[o].options[i]
             optionsEl.id = parseInt(o)
             optionsEl.append(option)
         }
-
-        questionWrapperEl.append(questionEl,optionsEl)
-
-    }
-
+    questionWrapperEl.append(questionEl,optionsEl)
     document.querySelector(".mainView").replaceWith(questionWrapperEl);
+    }else if(o === listQUestions.length){
+        console.log("showResult")
+    }
 }
 
 function checkMyAnswer(answer,id){
@@ -96,9 +98,17 @@ function checkMyAnswer(answer,id){
     answer.innerHTML = `<h1>${text}</h1>`
     mainWrapEl.appendChild(answer)
     
+    var t = 0
+    var wait = setInterval(function(){
+        t++;
+        displayQuestions();
+        answer.remove()
+        if(t === 1){
+            clearInterval(wait)
+        }
+    },700) 
+
 }
-
-
 startQuizEl.addEventListener("click",timer)//starting the quiz
 
 
@@ -109,9 +119,7 @@ startQuizEl.addEventListener("click",timer)//starting the quiz
 
 mainWrapEl.addEventListener("click",function(event){
     if(event.target.matches(".options")){
-        
-        // console.log(event.target.textContent)
-        // console.log(event.target.parentNode.getAttribute("id"))
-        checkMyAnswer(event.target.textContent,parseInt(event.target.parentNode.getAttribute("id")))
+        var text  = event.target.textContent;      
+        checkMyAnswer(text.substring(3,text.length),parseInt(event.target.parentNode.getAttribute("id")))
     }
 })
